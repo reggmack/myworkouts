b98615562ab6d3c96f513db4b0f082e55d76c8fa
reggmack <thegraphixninja@gmail.com>
2017-03-20 11:04:41 -0500

    Add Pages to Ionic UI
    Add Pages to Ionic UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,17 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
+import { AddWorkoutPage } from '../pages/add-workout/add-workout';
+import { WorkoutsPage } from '../pages/workouts/workouts';
+
 import { TabsPage } from '../pages/tabs/tabs';
 
 @NgModule({
   declarations: [
     MyApp,
     AboutPage,
-    ContactPage,
-    HomePage,
+    AddWorkoutPage,
+    WorkoutsPage,
     TabsPage
   ],
   imports: [
@@ -21,8 +22,8 @@ import { TabsPage } from '../pages/tabs/tabs';
   entryComponents: [
     MyApp,
     AboutPage,
-    ContactPage,
-    HomePage,
+    AddWorkoutPage,
+    WorkoutsPage,
     TabsPage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 import { HomePage } from '../home/home';
 import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
+import { AddWorkoutPage } from '../add-workout/add-workout';
+import { WorkoutsPage } from '../workouts/workouts';
 
 @Component({
   templateUrl: 'tabs.html'
@@ -10,9 +11,9 @@ import { ContactPage } from '../contact/contact';
 export class TabsPage {
   // this tells the tabs component which Pages
   // should be each tab's root Page
-  tab1Root: any = HomePage;
-  tab2Root: any = AboutPage;
-  tab3Root: any = ContactPage;
+  tab1Root: any = WorkoutsPage;
+  tab2Root: any = AddWorkoutPage;
+  tab3Root: any = AboutPage;
 
   constructor() {
 
